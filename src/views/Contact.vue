1dae56618652fc3228918394e00a770b9ba3f5a1
fatima galantls <fatimagalant03@gmail.com>
2022-07-22 15:11:23 +0200

    updates to mobile responsivenes

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -3,64 +3,69 @@
     <div class="container">
       <h1>Contact Me</h1>
       <div class="row">
-        <div class="col-6">
+        <div class="col-6 p-3">
           <div class="row">
-            <form action="https://formspree.io/f/moqrgdro" method="POST">
-              <div class="col">
-                <input
-                  type="text"
-                  class="form-control mb-2 bg-dark text-light"
-                  placeholder="name"
-                  aria-label="name"
-                  name="name"
-                />
-              </div>
-              <div class="col">
-                <input
-                  type="email"
-                  class="form-control mb-2 bg-dark text-light"
-                  placeholder="email"
-                  aria-label="email"
-                  name="email"
-                  required
-                />
-              </div>
-              <select
-                class="form-select mb-2 bg-dark text-light"
-                aria-label="subject"
-                name="subject"
-                placeholder="subject"
-              >
-                <option selected>subject</option>
-                <option value="1">query</option>
-                <option value="2">work opportunity</option>
-                <option value="3">personal</option>
-              </select>
-              <div class="mb-3">
-                <textarea
-                  class="form-control bg-dark text-light"
-                  id="exampleFormControlTextarea1"
-                  rows="3"
-                  name="message"
-                  placeholder="message"
-                ></textarea>
-              </div>
-              <button type="submit" class="btn btn-outline-light">
-                Submit
-              </button>
-            </form>
+            <div class="form-container">
+              <form action="https://formspree.io/f/moqrgdro" method="POST">
+                <div class="col">
+                  <input
+                    type="text"
+                    class="form-control mb-2 bg-dark text-light"
+                    placeholder="name"
+                    aria-label="name"
+                    name="name"
+                  />
+                </div>
+                <div class="col">
+                  <input
+                    type="email"
+                    class="form-control mb-2 bg-dark text-light"
+                    placeholder="email"
+                    aria-label="email"
+                    name="email"
+                    required
+                  />
+                </div>
+                <select
+                  class="form-select mb-2 bg-dark text-light"
+                  aria-label="subject"
+                  name="subject"
+                  placeholder="subject"
+                >
+                  <option selected>subject</option>
+                  <option value="1">query</option>
+                  <option value="2">work opportunity</option>
+                  <option value="3">personal</option>
+                </select>
+                <div class="mb-3">
+                  <textarea
+                    class="form-control bg-dark text-light"
+                    id="exampleFormControlTextarea1"
+                    rows="3"
+                    name="message"
+                    placeholder="message"
+                  ></textarea>
+                </div>
+                <button type="submit" class="btn btn-outline-light">
+                  Submit
+                </button>
+              </form>
+            </div>
           </div>
         </div>
         <div class="col-6">
-          <iframe
-            src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13233.069675818386!2d18.4926986!3d-33.9856643!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x9c746d268c8e0ad9!2sLife%20Choices!5e0!3m2!1sen!2sza!4v1658310399082!5m2!1sen!2sza"
-            width="450"
-            height="300"
-            style="border: 0"
-            allowfullscreen=""
-            loading="lazy"
-            referrerpolicy="no-referrer-when-downgrade"
-          ></iframe>
+          <div class="map-container m-2">
+            <iframe
+              class="p-2"
+              src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13233.069675818386!2d18.4926986!3d-33.9856643!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x9c746d268c8e0ad9!2sLife%20Choices!5e0!3m2!1sen!2sza!4v1658310399082!5m2!1sen!2sza"
+              width="450"
+              height="300"
+              style="border: 0"
+              allowfullscreen=""
+              loading="lazy"
+              referrerpolicy="no-referrer-when-downgrade"
+            ></iframe>
+          </div>
           <h1 class="address">
             Location: 314 Imam Haron Rd, Lansdowne, Cape Town, 7780
           </h1>
@@ -93,6 +98,19 @@ export default {
   justify-content: center;
   z-index: 1;
 }
+form {
+  width: 100%;
+  padding-left: 4rem;
+}
+.map-container {
+  width: 90%;
+  margin-top: 2rem;
+  height: 25rem;
+}
+iframe {
+  width: 100%;
+  height: 100%;
+}
 .container {
   /* border: 2px solid rgb(0, 0, 0); */
   z-index: 2;
@@ -102,7 +120,7 @@ export default {
   z-index: 2;
   padding-bottom: 193px;
   /* padding-top: 141px; */
-} 
+}
 @media screen and (max-width: 935px) {
   .col-6 {
     flex: 0 0 auto;
@@ -142,6 +160,7 @@ export default {
 button {
   display: flex;
   justify-content: center;
+  width: 74%;
 }
 .form-select {
   display: block;
@@ -181,11 +200,10 @@ iframe {
   filter: invert();
 }
 @media screen and (max-width: 467px) {
-  iframe {
-    display: none;
-  }
-  .address {
-    display: block;
+  .map-container {
+    width: 100%;
+    margin-top: 2rem;
+    height: 20rem;
   }
 }
 </style>
